1febe9426ce43969df811c7e5ea6d6bc74b4a6e1
Afnan153 <140311940+Afnan153@users.noreply.github.com>
2023-08-03 05:53:45 +0300

    Add files via upload

---
+++ 4/4/_App.swift
@@ -0,0 +1,17 @@
+//
+//  _App.swift
+//  4
+//
+//  Created by afnan althobaiti on 16/01/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct _App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
